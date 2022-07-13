365a06cf3a4e93fcc059660f56052496bff46035
Péricles Pires <periclespires@protonmail.com>
2022-07-13 01:25:30 -0300

    Adicionado DashboardKit Bootstrap

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
+  basePath: '/sistema',
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['images.pexels.com']
+  }
 }
 
 module.exports = nextConfig
