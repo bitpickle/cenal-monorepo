f1613d2c9be02176ea1a8d5528ef80406268b146
Péricles Pires <periclespires@protonmail.com>
2022-07-13 01:54:12 -0300

    Exemplo de pagina

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Cenal</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <div className="col-sm-12">
+          <div className="card">
+              <div className="card-header">
+                  <h5>Hello card</h5>
+              </div>
+              <div className="card-body">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                  </p>
+              </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
